54f0e67d6ac4e691cbcf337c31192426a1d7811e
Max Grossman <jmaxg3@gmail.com>
2015-11-21 18:41:55 -0600

    add concept of assignments, enabled or disabled

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var pg = require('pg');
 
 var ejs = require('ejs');
 
+var permissionDenied = 'Permission denied. But you should shoot me an e-mail at jmaxg3@gmail.com. If you like playing around with systems, we have interesting research for you in the Habanero group.';
+
 // TODO load this from JSON file
 var conString = "postgres://postgres:foobar@localhost/autograder";
 
@@ -19,6 +21,16 @@ function pgclient(cb) {
         });
 }
 
+function register_query_helpers(query, res, done, username) {
+    query.on('row', function(row, result) { result.addRow(row); });
+    query.on('error', function(err, result) {
+            done();
+            res.send(JSON.stringify({ status: 'Failure',
+                    msg: 'Internal error (' + err + ')', user: username }));
+    });
+
+}
+
 var app = express();
 app.use(bodyParser.urlencoded());
 app.use(session({secret: 'blarp', cookie:{maxAge: 7 * 24 * 3600 * 1000}}));
@@ -27,7 +39,7 @@ app.set('view engine', 'html');
 app.set('views', __dirname + "/views");
 
 /*
- * login routes should always be the only routes above the wildcard '*' route
+ * login/logout routes should always be the only routes above the wildcard '*' route
  */
 app.get('/login', function(req, res, next) {
   res.render('login.html');
@@ -42,12 +54,7 @@ app.post('/login', function(req, res, next) {
   // Check that user exists
   pgclient(function(client, done) {
         var query = client.query("SELECT * FROM users WHERE user_name=($1)", [username]);
-        query.on('row', function(row, result) { result.addRow(row); });
-        query.on('error', function(err, result) {
-              done();
-              res.send(JSON.stringify({ status: 'Failure',
-                      msg: 'Internal error (' + err + ')', user: username }));
-            });
+        register_query_helpers(query, res, done, username);
         query.on('end', function(result) {
               done();
               if (result.rowCount == 0) {
@@ -58,6 +65,8 @@ app.post('/login', function(req, res, next) {
                 // Check that password matches
                 if (bcrypt.compareSync(password, result.rows[0].password_hash)) {
                   req.session.username = username;
+                  req.session.is_admin = result.rows[0].is_admin;
+
                   res.send(JSON.stringify({ status: 'Success',
                           redirect: '/overview', user: username }));
                 } else {
@@ -73,22 +82,122 @@ app.post('/login', function(req, res, next) {
       });
 });
 
+app.get('/logout', function(req, res, next) {
+  console.log('logout: username=' + req.session.username);
+
+  req.session.username = null;
+  req.session.is_admin = false;
+
+  res.render('login.html');
+});
+
 app.get('*', function(req, res, next) {
   if (req.session.username) {
+    res.locals.username = req.session.username;
+    res.locals.is_admin = req.session.is_admin;
+
     next();
   } else {
+    res.locals.username = null;
+    res.locals.is_admin = false;
+
     res.redirect('/login');
   }
 });
 
-app.get('/logout', function(req, res, next) {
-  console.log('logout: username=' + req.session.username);
-  req.session.username = null;
-  res.render('login.html');
-});
-
 app.get('/overview', function(req, res, next) {
   res.render('overview.html');
+});
+
+app.get('/admin', function(req, res, next) {
+  if (req.session.is_admin) {
+    res.render('admin.html');
+  } else {
+    res.redirect('/overview');
+  }
+});
+
+// Create a new assignment with a given name, not visible to students
+app.post('/assignment', function(req, res, next) {
+  console.log('assignment: is_admin=' + req.session.is_admin);
+  if (!req.session.is_admin) {
+    res.send(JSON.stringify({ status: 'Failure', msg: permissionDenied }));
+  } else {
+    var assignment_name = req.body.assignment_name;
+
+    pgclient(function(client, done) {
+          var query = client.query(
+              "INSERT INTO assignments (name, visible) VALUES ($1,false)",
+              [assignment_name]);
+          register_query_helpers(query, res, done, req.session.username);
+          query.on('end', function(result) {
+              done();
+              res.send(JSON.stringify({ status: 'Success', redirect: '/admin' }));
+          });
+    });
+  }
+});
+
+/*
+ * Get all assignments, with an optional flag to also view not visible
+ * assignments (which is only legal for admin users, and ignored for all others).
+ */
+app.get('/assignments', function(req, res, next) {
+  var get_not_visible = false;
+  if (req.query.get_not_visible && req.session.is_admin) {
+    get_not_visible = true;
+  }
+  console.log('assignments: username=' + req.session.username +
+      ' get_not_visible=' + get_not_visible);
+
+  pgclient(function(client, done) {
+    var query = null;
+    if (get_not_visible) {
+        query = client.query("SELECT * FROM assignments ORDER BY assignment_id ASC");
+    } else {
+        query = client.query("SELECT * FROM assignments WHERE visible=true;");
+    }
+    register_query_helpers(query, res, done, req.session.username);
+    query.on('end', function(result) {
+        done();
+        console.log('assignments: returning ' + result.rowCount + ' assignment(s)');
+        res.send(JSON.stringify({ status: 'Success',
+                'assignments': result.rows }));
+    });
+  });
+});
+
+app.post('/set_assignment_visible', function(req, res, next) {
+  console.log('set_assignment_visible: is_admin=' + req.session.is_admin);
+  if (!req.session.is_admin) {
+    res.send(JSON.stringify({ status: 'Failure', msg: permissionDenied }));
+  } else {
+    var assignment_id = req.body.assignment_id;
+    var set_visible = req.body.set_visible;
+
+    pgclient(function(client, done) {
+        var query = client.query(
+            "SELECT * FROM assignments WHERE assignment_id=($1);",
+            [assignment_id]);
+        register_query_helpers(query, res, done, req.session.username);
+        query.on('end', function(result) {
+            if (result.rowCount == 0) {
+                return res.send(JSON.stringify({ status: 'Failure',
+                        msg: 'Invalid assignment ID, does not exist' }));
+            } else {
+                var query = client.query(
+                    "UPDATE assignments SET visible=($1) WHERE assignment_id=($2);",
+                    [set_visible, assignment_id]);
+                register_query_helpers(query, res, done, req.session.username);
+                query.on('end', function(result) {
+                    return res.send(JSON.stringify({ status: 'Success',
+                            redirect: '/admin' }));
+                });
+            }
+        });
+    });
+
+  }
 });
 
 var port = process.env.PORT || 8000
