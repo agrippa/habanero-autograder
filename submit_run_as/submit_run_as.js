5725591298484ec1ea3ffb1553e47e82abb7d882
Max Grossman <jmaxg3@gmail.com>
2016-02-28 11:17:50 -0600

    extend submit run script to provide student who can view the run

--- submit_run_as/submit_run_as.js
+++ submit_run_as/submit_run_as.js
@@ -1,17 +1,19 @@
 var request = require('request');
 
-if (process.argv.length != 5) {
-    console.log('usage: node submit_run_as.js <username> <assignment-name> <svn_url>');
+if (process.argv.length != 6) {
+    console.log('usage: node submit_run_as.js <as-username> <for-username> <assignment-name> <svn_url>');
     process.exit(1);
 }
 
 var username = process.argv[2];
-var assignment_name = process.argv[3];
-var svn_url = process.argv[4];
+var for_username = process.argv[3];
+var assignment_name = process.argv[4];
+var svn_url = process.argv[5];
 
 request({
     url: 'http://ananke.cs.rice.edu/submit_run_as?svn_url=' + svn_url +
-         '&username=' + username + '&assignment_name=' + assignment_name,
+         '&username=' + username + '&assignment_name=' + assignment_name +
+        '&for_username=' + for_username,
     method: "POST",
 }, function(err, response, body) {
     if (err) {
