478316076bc4b8ffd1ec422b0f06b33b77ec7b62
Max Grossman <jmaxg3@gmail.com>
2016-03-18 13:39:35 -0500

    remove reference to svn jars

--- viola/deploy.sh
+++ viola/deploy.sh
@@ -12,10 +12,6 @@ CONDUCTOR_PORT=$2
 CONDUCTOR_USER=$3
 
 JARS=".:commons-io-2.4.jar"
-for F in $(ls lib/svnkit-1.8.11/lib/*.jar); do
-    JARS="$JARS:$F"
-done
-
 echo $JARS
 
 java -classpath $JARS Viola 8080 $CONDUCTOR_HOST $CONDUCTOR_PORT $CONDUCTOR_USER
