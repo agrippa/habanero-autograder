482694f0e6f35c4e9d320dd36b66fc47ce0fb708
Max Grossman <jmaxg3@gmail.com>
2016-03-27 08:47:51 -0500

    fix up conductor and viola deploy scripts to automatically save old logs

---
+++ conductor/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOG_DIR=$SCRIPT_DIR/conductor-logs
+
+LOG_FILE_INDEX=1
+
+while [[ -f $LOG_DIR/conductor.${LOG_FILE_INDEX}.out || \
+         -f $LOG_DIR/conductor.${LOG_FILE_INDEX}.err ]]; do
+    LOG_FILE_INDEX=$((LOG_FILE_INDEX + 1))
+done
+
+STDOUT_LOG_FILE=$LOG_DIR/conductor.${LOG_FILE_INDEX}.out
+STDERR_LOG_FILE=$LOG_DIR/conductor.${LOG_FILE_INDEX}.err
+echo Logging STDOUT to $STDOUT_LOG_FILE, STDERR to $STDERR_LOG_FILE
+
+PORT=8081 node $SCRIPT_DIR/app.js 2> $STDERR_LOG_FILE 1> $STDOUT_LOG_FILE
--- viola/deploy.sh
+++ viola/deploy.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOG_DIR=$SCRIPT_DIR/viola-logs
+
 if [[ $# != 3 ]]; then
     echo 'usage: deploy.sh conductor-host conductor-port conductor-user'
     exit 1
@@ -11,7 +14,16 @@ CONDUCTOR_HOST=$1
 CONDUCTOR_PORT=$2
 CONDUCTOR_USER=$3
 
-JARS=".:commons-io-2.4.jar"
-echo $JARS
+LOG_FILE_INDEX=1
 
-java -classpath $JARS Viola 8080 $CONDUCTOR_HOST $CONDUCTOR_PORT $CONDUCTOR_USER
+while [[ -f $LOG_DIR/viola.${LOG_FILE_INDEX}.out || \
+         -f $LOG_DIR/viola.${LOG_FILE_INDEX}.err ]]; do
+    LOG_FILE_INDEX=$((LOG_FILE_INDEX + 1))
+done
+
+STDOUT_LOG_FILE=$LOG_DIR/viola.${LOG_FILE_INDEX}.out
+STDERR_LOG_FILE=$LOG_DIR/viola.${LOG_FILE_INDEX}.err
+echo Logging STDOUT to $STDOUT_LOG_FILE, STDERR to $STDERR_LOG_FILE
+
+java -classpath .:commons-io-2.4.jar Viola 8080 $CONDUCTOR_HOST \
+    $CONDUCTOR_PORT $CONDUCTOR_USER 2> $STDERR_LOG_FILE 1> $STDOUT_LOG_FILE
