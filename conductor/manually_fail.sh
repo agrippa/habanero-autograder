140b61c31c5ea82e8d76ca236d5e5536ac6e5594
Max Grossman <jmaxg3@gmail.com>
2016-04-04 18:45:59 -0500

    add a script for manually failing jobs

---
+++ conductor/manually_fail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+if [[ $# != 1 ]]; then
+    echo usage: manually_fail.sh run-id
+    exit 1
+fi
+
+psql --dbname=autograder --user=postgres --command="UPDATE runs SET status='FAILED',finish_time=CURRENT_TIMESTAMP,cello_msg='Failed on server restart' WHERE run_id=$1;"
