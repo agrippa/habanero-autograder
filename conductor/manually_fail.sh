ea4552708364252fc3c226d7852b93bbf033064f
Max Grossman <jmaxg3@gmail.com>
2016-04-04 21:12:42 -0500

    dont display all runs in history graph

--- conductor/manually_fail.sh
+++ conductor/manually_fail.sh
@@ -7,4 +7,4 @@ if [[ $# != 1 ]]; then
     exit 1
 fi
 
-psql --dbname=autograder --user=postgres --command="UPDATE runs SET status='FAILED',finish_time=CURRENT_TIMESTAMP,cello_msg='Failed on server restart' WHERE run_id=$1;"
+psql --dbname=autograder --user=postgres --command="UPDATE runs SET status='FAILED',finish_time=CURRENT_TIMESTAMP,cello_msg='Failed on server restart, please resubmit.' WHERE run_id=$1;"
